adc2abce69617151e739bc3bf39aff138051e30e
unknown <scheng52123@gmail.com>
2021-12-17 23:02:49 +0800

    add repeater.js

---
+++ utils/repeater.js
@@ -0,0 +1,13 @@
+import config from '../config/config.js';
+import helper from '../helper/helper.js';
+import player from '@/utils/player.js';
+import storage from '@/utils/storage.js';
+import song from '../model/song.js';
+
+export default {
+	config,
+	helper,
+	player,
+	storage,
+	song,
+}
