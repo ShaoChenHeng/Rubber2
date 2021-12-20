5b8d12b3a69ca5c2774216e4e9360729367596bf
unknown <scheng52123@gmail.com>
2021-12-20 22:28:37 +0800

    Add logic to play_mode.

--- store/index.js
+++ store/index.js
@@ -3,10 +3,10 @@ import Vuex from 'vuex';
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  state: {
+	state: {
 		played: '', 
 		playing: false, // 全局播放状态
-		
+		current_page: 0,
 		play_mode: 'sequence', //single, random, sequence 
 		uid: '', //网易云用户id
 		user_info: {}, // 网易云用户详情
@@ -15,21 +15,65 @@ const store = new Vuex.Store({
 		
 		current_playlist: [], // 当前播放列表, 应该两种模式, 播放历史和歌曲所在清单
 	},
-  mutations: {
-    set_play_mode(state, mode) {
-			state.play_mode = mode;
-			Storage.set_play_mode(state.play_mode);
+	mutations: {
+		change_page(state, page_index) {
+			state.current_page = page_index;
 		},
-    set_current_playlist(state, item) {
-      state.current_playlist.push(item);
+		push_current_playlist(state, item) {
+			const index = state.current_playlist.findIndex(itemInList => {
+				return itemInList === item;
+			})
+			console.log(index)
+			if (index === -1) {
+				console.log('in push');
+				state.current_playlist.push(item);
+				Storage.set_current_playlist(state.current_playlist);	
+			}			
+		},
+		set_current_playlist(state, playlist) {
+			state.current_playlist = playlist;
 			Storage.set_current_playlist(state.current_playlist);
-    },
-    set_playing(state, playing) {
-			state.playing = playing;
 		},
-    set_played(state, played) {
+		set_played(state, played) {
 			state.played = played;
 			Storage.set_played(played);
 		},
-  }
+		
+		set_playing(state, playing) {
+			state.playing = playing;
+		},
+		set_uid(state, uid) {
+			state.uid = uid;
+		},
+		set_user_info(state, user_info) {
+			state.user_info = user_info;
+		},
+		set_subcount(state, subcount) {
+			state.subcount = subcount;
+		},
+		set_likelist(state, likelist) {
+			state.likelist = likelist;
+		},
+		logout(state) {
+			state.played = '';
+			state.playing = false;
+			state.uid = '';
+			state.user_info = {};
+			state.subcount = {}; // 各种计数
+			state.likelist = [];
+			state.current_playlist = [];
+			Storage.set_is_login(false);
+		},
+		login(state, uid) {
+			state.uid = uid;
+			Storage.set_is_login(true);
+		},
+		set_play_mode(state, mode) {
+			state.play_mode = mode;
+			Storage.set_play_mode(state.play_mode);
+		}
+	}
 })
+
+import Storage from '../utils/storage.js';
+export default store
--- utils/player.js
+++ utils/player.js
@@ -7,28 +7,37 @@ function get_player() {
 }
 
 PlayerHelper.set_url = function(url) {
+	
 	url = url || Storage.get_played().url;
 	// console.log('set_url:' + url);
 	let player = get_player();
 	player.setStyles({
 		src: url
 	});
-
+	
 	return;
 }
+
+//play前的准备
+// PlayerHelper.start = function() {
+// 	this.set_url();
+// 	let player = get_player();
+// 	player.resume();
+// 	return;
+// }
 
 //统一play入口, 真正的播放入口
 PlayerHelper.play = function() {
 	let player = get_player();
-	player.play(() => {
+	player.play(()=>{
 		console.log('当前音乐播放完毕');
-	}, (e) => {
+	},(e)=> {
 		console.log(e);
 	});;
 	return;
 }
 
-/*
+/**
  * 暂停
  * 需要start后, 触发onPlay事件后, pause才有效
  */
@@ -45,57 +54,12 @@ PlayerHelper.get_position = function() {
 
 PlayerHelper.get_duration = function() {
 	let player = get_player();
-
+	
 	return player.getDuration();
 }
 
 PlayerHelper.seek_to = function(position) {
 	return get_player().seekTo(position);
-}
-
-PlayerHelper.prev = function(cb) {
-	let played = Storage.get_played();
-	
-	let playlist = Storage.get_current_playlist();
-	console.log(playlist)
-	let current_song_id = played.id;
-	let prev_index = playlist.length - 1;
-
-	for (let i = 0; i < playlist.length; i++) {
-		if (current_song_id == playlist[i].id) {
-			if (i > 0) {
-				prev_index = i - 1;
-			}
-			break;
-		}
-	}
-
-	let prev_song_id = playlist[prev_index].id;
-	this.start(prev_song_id, (played) => {
-		cb && cb(played)
-	});
-}
-
-
-PlayerHelper.next = function(cb) {
-	let played = Storage.get_played();
-	let current_song_id = played.id;
-	let playlist = Storage.get_current_playlist();
-
-	let next_index = 0;
-	for (let i = 0; i < playlist.length; i++) {
-		if (current_song_id == playlist[i].id) {
-			if (i + 1 < playlist.length) {
-				next_index = i + 1;
-			}
-			break;
-		}
-	}
-	console.log(next_index);
-	let next_song_id = playlist[next_index].id;
-	this.start(next_song_id, (played) => {
-		cb && cb(played)
-	});
 }
 
 
@@ -106,75 +70,133 @@ PlayerHelper.next = function(cb) {
 PlayerHelper.start = function(song_id, cb) {
 	let player = get_player();
 	let played = Storage.get_played();
-
-	Song.get_song_detail(song_id, (data) => {
+	
+	Song.get_song_detail(song_id, (data)=> {
 		console.log(data);
 		let song = data.songs[0];
 		let song_name = song.name;
 		let cover_image = song.al.picUrl;
 		let current_played = played;
 		let played_music_id = current_played.id;
-
+		
 		if (song_id == played_music_id) {
 			// 跳转播放详情页并播放
 			let player = get_player();
 			if (player.isPaused()) {
-				player.play(() => {}, (e) => {
-
+				player.play(()=>{},(e)=> {
+					
 				});;
 			}
 			// this.turn();
-			Helper.to('../play/play');
+			let routes = getCurrentPages();
+			let curRoute = routes[routes.length - 1].route;
+			if (curRoute != 'pages/play/play') Helper.to('../play/play');	
+			
 			return;
 		}
-
-		Song.get_song_url(song_id, (res) => {
+		
+		Song.get_song_url(song_id, (res)=> {
 			console.log(res);
 			let url = res.data[0].url;
 			console.log(url);
 			if (!url) {
 				setTimeout(function() {
-					Helper.toast('none', '暂无该歌曲资源~', 3000, false, 'bottom');
+					Helper.toast('none', '亲爱的, 暂无该歌曲资源~', 3000, false, 'bottom');
 				}, 10);
 				return;
 			}
-
+			
+			
+			
 			let creators = song.ar;
 			let creator_str = '';
+			
 			for (let creator of creators) {
 				creator_str = creator_str + creator.name;
 			}
-
+			
 			let played = {
 				id: song_id,
 				url: url,
 				name: song_name,
 				creator: creator_str,
 				cover_image: cover_image,
-
-				// time: 0,
-				//上次播放的位置
+				// time: 0, //上次播放的位置
 			}
-
+			
 			this.set_url(url);
+			
 			cb && cb(played);
+			
 		});
-
+		
 	});
 }
 
 PlayerHelper.turn = function() {
+	console.log('in turn')
 	let player = get_player();
 	if (player.isPaused()) {
-		player.play(() => {}, (e) => {
+		player.play(()=>{},(e)=> {
 			console.log(e);
 		});;
-	} else {
+	}else {
 		player.pause();
 	}
 	return;
 }
 
+PlayerHelper.prev = function(cb) {
+	let played = Storage.get_played();
+	let playlist = Storage.get_current_playlist();
+	let current_song_id = played.id;
+	let prev_index = playlist.length - 1;
+	
+	for (let i = 0; i < playlist.length; i++) {
+		if (current_song_id == playlist[i].id) {
+			if (i > 0) {
+				prev_index = i - 1;
+			}
+			break;
+		}
+	}
+	
+	let prev_song_id = playlist[prev_index].id;
+	this.start(prev_song_id, (played)=> {
+		cb && cb(played)
+	});
+}
+
+
+PlayerHelper.next = function(play_mode, cb) {
+	let played = Storage.get_played();
+	let current_song_id = played.id;
+	let playlist = Storage.get_current_playlist();
+	let next_index = 0;
+	let current_index = 0;
+	for (let i = 0; i < playlist.length; i++) {
+		if (current_song_id == playlist[i].id) {
+			current_index = i;
+		}
+	}
+	console.log(play_mode);
+	if (play_mode == 'single') {
+		next_index = current_index;
+	} else if (play_mode == 'sequence') {
+		if (current_index + 1 >= playlist.length) next_index = 0;
+		else next_index = current_index + 1;
+	} else if (play_mode == 'random') {
+		next_index = Math.round(Math.random()*playlist.length);
+	}
+	
+	console.log(next_index);
+	let next_song_id = playlist[next_index].id;
+	this.start(next_song_id, (played)=> {
+		cb && cb(played)
+	});
+}
+
+import Storage from '../utils/storage.js'
 import Song from '../model/song.js';
 import Helper from '../helper/helper.js'
 
