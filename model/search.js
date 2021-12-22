f6ebc5e0b3a86289733f5eb6977840f7236f3fa0
unknown <scheng52123@gmail.com>
2021-12-22 16:09:38 +0800

    Add more search function.

--- model/search.js
+++ model/search.js
@@ -1,9 +1,60 @@
 let Search = {};
 
+// 获取排行榜
 Search.get_hot_list = function(cb) {
 	let url = config.API.HOT_SEARCH;
 
 	request.get(url, {}, (res)=> {
+		let data = res.data
+		if (data.code == 200) {
+			cb && cb(data)
+		}else {
+			console.log('failed');
+		}
+	})
+}
+
+// 获取最新歌曲（单曲）
+Search.get_new_songs = function(cb) {
+	let url = config.API.NEW_SONGS;
+
+	request.get(url, {}, (res)=> {
+		let data = res.data
+		if (data.code == 200) {
+			cb && cb(data)
+		} else {
+			console.log('failed');
+		}
+	})
+}
+
+// 获取热门专辑
+
+/* limit: 取出数量 , 默认为 50
+
+offset: 偏移数量 , 用于分页 , 如 :( 页数 -1)*50, 其中 50 为 limit 的值 , 默认 为 0
+
+area: ALL:全部,ZH:华语,EA:欧美,KR:韩国,JP:日本
+
+type : new:全部 hot:热门,默认为 new
+
+year : 年,默认本年
+
+month : 月,默认本月 */
+Search.get_top_albums = function(cb, limit, offset, area, type) {
+	let url = config.API.TOP_ALBUM;
+	limit = limit 	|| 20;
+	offset = offset || 0;
+	area = area 	|| 'ALL';
+	type = type 	|| 'hot';
+	
+	let params = {
+		limit: limit,
+		offset: offset,
+		area: area,
+		type: type
+	}
+	request.get(url, params, (res)=> {
 		let data = res.data
 		if (data.code == 200) {
 			cb && cb(data)
