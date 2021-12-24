9223fb6b711060b033e7c13f0c36171947cc3309
unknown <scheng52123@gmail.com>
2021-12-24 20:40:37 +0800

    Add APIs, sorry I fogort this before.

--- config/config.js
+++ config/config.js
@@ -1,16 +1,60 @@
-const HOST = 'https://netease-cloud-music-api-teal-tau.vercel.app/';
+
 const systemInfo = uni.getSystemInfoSync();
+
 const status_bar_height = systemInfo.statusBarHeight;
 
+const HOST = 'https://netease-cloud-music-api-teal-tau.vercel.app/';
 
 const API = {};
+//登录
+API.CELLPHONE_LOGIN = 'login/cellphone'; // 手机号登录
+API.LOGIN_STATUS = 'login/status'; // 登陆状态
+API.CHECK_PHONE = 'cellphone/existence/check'; // 检查手机号是否注册过
+API.LOGOUT = 'logout'; // 登出
+API.SEND_CAPTCHA = 'captcha/sent'; // 发送短信验证码
+API.VERIFY_CAPTCHA = 'captcha/verify'; // 核实验证码
+API.PHONE_REG = 'register/cellphone'; // 手机号注册
+//用户
+API.SUBCOUNT = 'user/subcount'; // 用户的歌单，收藏，mv, dj 数量
+API.USER_DETAIL = 'user/detail'; // 获取用户详情
+API.USER_PLAYLIST = 'user/playlist'; // 获取用户歌单
+API.USER_LIKE_LIST = 'likelist'; // 获取用户喜欢的音乐列表
+API.LIKE = 'like'; // 喜欢某个歌曲
 
-// 获取歌曲的url
-API.SONG = 'song/url'
-// 获取歌曲的歌词
-API.LYRIC = 'lyric';
-// 获取歌曲的详细信息
-API.SONG_DETAIL = 'song/detail';
+// home
+API.NEW_SONGS = 'top/song?type=0'//0: 全部新歌, 7: 华语, 96: 欧美,日本:8
+
+// 歌单
+API.PLAYLIST_DETAIL = 'playlist/detail'; // 歌单详情
+
+// 专辑
+API.ALBUM_DETAIL = 'album'; //专辑详情
+
+// 歌曲
+API.SONG = 'song/url'; //获取歌曲url
+API.LYRIC = 'lyric'; //获取歌词
+API.SONG_DETAIL = 'song/detail'; //获取歌曲详情
+
+// 搜索
+API.HOT_SEARCH = 'search/hot/detail'; //获取热搜
+API.SEARCH = 'search'; //搜索
+
+
+// Other
+API.BANNER = 'banner'; // 发现页banner图
+
+// 私人推荐
+API.PERSONALIZED_PLAYLIST = 'personalized'; // 推荐歌单
+API.PERSONALIZED_NEWSONG = 'personalized/newsong'; // 推荐新歌
+API.RECOMMEND_RESOURCE = 'recommend/resource'; // 每日推荐歌单
+API.RECOMMEND_SONGS = 'recommend/songs'; // 每日推荐歌单
+API.FM = '/personal_fm' //私人FM
+
+// 新碟上架
+API.TOP_ALBUM = 'top/album'; 
+
+// 视频
+API.VIDEO_LIST = 'video/group'; // 按分类获取视频列表, 需登录
 
 
 export default {
